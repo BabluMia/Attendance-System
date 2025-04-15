16a97c34fc4bc1dd6fd66667673c061c8f640b1e
Bablu Mia <69855560+BabluMia@users.noreply.github.com>
2025-04-15 23:08:36 +0600

    first commit

---
+++ src/pages/(hdr-krh)/(int)/(agency)/agency.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <AgencySearch />
+    <AgencyList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AgencySearch from "../components/AgencySearch.vue";
+import AgencyList from "../components/AgencyList.vue";
+</script>
